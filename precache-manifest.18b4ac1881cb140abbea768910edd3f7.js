253e798404f18b6f26558a3356b0674807a4288b
Your Name <you@example.com>
2020-05-29 23:03:04 +0530

    Updates

---
+++ precache-manifest.18b4ac1881cb140abbea768910edd3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a048ed21ded11383df88fd06225a969",
+    "url": "/ReactTextFilter/index.html"
+  },
+  {
+    "revision": "ace416acc8cd0d1dc18d",
+    "url": "/ReactTextFilter/static/css/main.d6b9b328.chunk.css"
+  },
+  {
+    "revision": "2c643acc1b1026722189",
+    "url": "/ReactTextFilter/static/js/2.f9462cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTextFilter/static/js/2.f9462cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace416acc8cd0d1dc18d",
+    "url": "/ReactTextFilter/static/js/main.808b4387.chunk.js"
+  },
+  {
+    "revision": "d612067f840e32598ef8",
+    "url": "/ReactTextFilter/static/js/runtime-main.47b9f3a8.js"
+  }
+]);
